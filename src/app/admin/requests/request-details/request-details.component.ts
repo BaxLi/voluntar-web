80a48415c70b77199cf88f3297029532e1c5e60e
DanielBaxan <56045886+DanielBaxan@users.noreply.github.com>
2020-12-18 12:19:23 +0200

    Updated CSS (#7)
    * Updated CSS
    
    * prettier cfg
    
    * map-001
    
    * Updated CSS
    
    * Updated request-details-component
    
    Co-authored-by: BaxLi <lilian.baxan@gmail.com>

--- src/app/admin/requests/request-details/request-details.component.ts
+++ src/app/admin/requests/request-details/request-details.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit
+} from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { SPECIAL_CONDITIONS, ZONES } from '@app/shared/constants'
 import { RequestsFacade } from '../requests.facade'
@@ -23,14 +28,32 @@ export enum CUSTOMER_NEEDS {
 export class RequestDetailsComponent implements OnInit, OnDestroy {
   form: FormGroup
   zones: Array<string> = Object.keys(ZONES).filter((key) => isNaN(+key))
-  needs: Array<string> = Object.values(CUSTOMER_NEEDS).filter((key) => isNaN(+key))
-  ilnessOptions: Array<string> = Object.values(ILNESS_OPTIONS).filter((key) => isNaN(+key))
+  needs: Array<string> = Object.values(CUSTOMER_NEEDS).filter((key) =>
+    isNaN(+key)
+  )
+  ilnessOptions: Array<string> = Object.values(ILNESS_OPTIONS).filter((key) =>
+    isNaN(+key)
+  )
   specialConditions = SPECIAL_CONDITIONS
+
+  testbool = true //Testing for the beneficiary component
+
   constructor(private requestsFacade: RequestsFacade) {}
 
   onSubmit(ev: Event) {
-    this.form.get('need').setValue(this.getEnumKeyByEnumValue(CUSTOMER_NEEDS, this.form.get('need').value))
-    this.form.get('ilness').setValue(this.getEnumKeyByEnumValue(ILNESS_OPTIONS, this.form.get('ilness').value))
+    this.form
+      .get('need')
+      .setValue(
+        this.getEnumKeyByEnumValue(CUSTOMER_NEEDS, this.form.get('need').value)
+      )
+    this.form
+      .get('ilness')
+      .setValue(
+        this.getEnumKeyByEnumValue(
+          ILNESS_OPTIONS,
+          this.form.get('ilness').value
+        )
+      )
   }
 
   ngOnInit(): void {
@@ -43,7 +66,11 @@ export class RequestDetailsComponent implements OnInit, OnDestroy {
       apartment: new FormControl(null),
       entrance: new FormControl(null),
       floor: new FormControl(null),
-      phone: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(8)]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(8)
+      ]),
       landline: new FormControl(
         null,
         // because we add prefix 22 at form submit.
