05c224f7cfba8c0efc783b04fa96baa141890610
DanielBaxan <56045886+DanielBaxan@users.noreply.github.com>
2020-12-24 11:37:36 +0200

    Dev daniel baxan (#8)
    * Updated CSS
    
    * Updated CSS
    
    * Updated request-details-component
    
    * Updated request-details component
    
    * Request-details update
    
    * Updated request-details-component
    
    * Almost finished HTML of the form

--- src/app/admin/requests/request-details/request-details.component.ts
+++ src/app/admin/requests/request-details/request-details.component.ts
@@ -13,12 +13,24 @@ export enum ILNESS_OPTIONS {
   'Has symptoms' = 'Are simptome'
 }
 
-export enum CUSTOMER_NEEDS {
-  'Food' = 'Produse Alimentare',
-  'Drugs' = 'Medicamente',
-  'Transport' = 'Transport Persoana',
-  'Payment' = 'Achitare Facturi'
-}
+export const BENEFICIARY_NEEDS = [
+  {
+    label: 'Food',
+    value: 'Produse Alimentare'
+  },
+  {
+    label: 'Drugs',
+    value: 'Medicamente'
+  },
+  {
+    label: 'Transport',
+    value: 'Transport Persoana'
+  },
+  {
+    label: 'Payment',
+    value: 'Achitare Facturi'
+  }
+]
 
 @Component({
   templateUrl: './request-details.component.html',
@@ -28,23 +40,25 @@ export enum CUSTOMER_NEEDS {
 export class RequestDetailsComponent implements OnInit, OnDestroy {
   form: FormGroup
   zones: Array<string> = Object.keys(ZONES).filter((key) => isNaN(+key))
-  needs: Array<string> = Object.values(CUSTOMER_NEEDS).filter((key) =>
-    isNaN(+key)
-  )
+  needs = BENEFICIARY_NEEDS
   ilnessOptions: Array<string> = Object.values(ILNESS_OPTIONS).filter((key) =>
     isNaN(+key)
   )
   specialConditions = SPECIAL_CONDITIONS
 
-  testbool = true //Testing for the beneficiary component
+  existentBeneficiary = false //Testing for the beneficiary component
 
   constructor(private requestsFacade: RequestsFacade) {}
 
   onSubmit(ev: Event) {
+    // Transform the values for backend
     this.form
       .get('need')
       .setValue(
-        this.getEnumKeyByEnumValue(CUSTOMER_NEEDS, this.form.get('need').value)
+        this.getEnumKeyByEnumValue(
+          BENEFICIARY_NEEDS,
+          this.form.get('need').value
+        )
       )
     this.form
       .get('ilness')
@@ -69,19 +83,25 @@ export class RequestDetailsComponent implements OnInit, OnDestroy {
       phone: new FormControl(null, [
         Validators.required,
         Validators.minLength(8),
-        Validators.maxLength(8)
+        Validators.maxLength(8),
+        Validators.pattern(/^([0-9]){8}$/)
       ]),
       landline: new FormControl(
         null,
         // because we add prefix 22 at form submit.
-        [Validators.required, Validators.minLength(6), Validators.maxLength(6)]
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(6),
+          Validators.pattern(/^([0-9]){6}$/)
+        ]
       ),
       special_condition: new FormControl(null, [Validators.required]),
       ilness: new FormControl('', [Validators.required]),
       need: new FormControl('', [Validators.required]),
       comments: new FormControl(''),
       password: new FormControl(null, [Validators.required]),
-      urgent: new FormControl(false, [Validators.required])
+      urgent: new FormControl(false)
     })
   }
   ngOnDestroy() {}
@@ -89,5 +109,20 @@ export class RequestDetailsComponent implements OnInit, OnDestroy {
   getEnumKeyByEnumValue(myEnum, enumValue) {
     let keys = Object.keys(myEnum).filter((x) => myEnum[x] == enumValue)
     return keys.length > 0 ? keys[0] : null
+  }
+
+  enumUnsorted() {}
+
+  checkForExistentBeneficiary(phone: any) {
+    // this function should display the hidden div if the beneficiary is found
+    // check if the logic works
+    if (phone.length == 8) this.existentBeneficiary = true
+    else this.existentBeneficiary = false
+  }
+
+  getUrgentStyleObject() {
+    if (this.form.get('urgent').value === false) {
+      return { backgroundColor: 'white', color: '#ed5555' }
+    } else return { backgroundColor: '#ed5555', color: 'white' }
   }
 }
