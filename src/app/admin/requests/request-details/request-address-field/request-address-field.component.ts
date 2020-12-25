06eb7c75c4b718c30d0d0f4bd6abbf72dbdbbdfb
DanielBaxan <daniel.baxan@gmail.com>
2020-12-25 11:42:30 +0200

    Updated adress field + added apart component

---
+++ src/app/admin/requests/request-details/request-address-field/request-address-field.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-request-address-field',
+  templateUrl: './request-address-field.component.html',
+  styleUrls: ['./request-address-field.component.scss']
+})
+export class RequestAddressFieldComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/admin/requests/requests.module.ts
+++ src/app/admin/requests/requests.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RequestsComponent } from './requests.component';
-import { RequestDetailsComponent } from './request-details/request-details.component';
-import { RequestsListComponent } from './requests-list/requests-list.component';
-import { MaterialComponentsModule } from '@shared/material.module';
-import { SharedModule } from '@shared/shared.module';
-import { RequestFormComponent } from './shared/request-form/request-form.component';
-import { NgxMaskModule } from 'ngx-mask';
-import { RequestsMapComponent } from './shared/requests-map/requests-map.component';
-import { RouterModule } from '@angular/router';
-import { requestsRoutes } from './requests.routes';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RequestsComponent } from './requests.component'
+import { RequestDetailsComponent } from './request-details/request-details.component'
+import { RequestsListComponent } from './requests-list/requests-list.component'
+import { MaterialComponentsModule } from '@shared/material.module'
+import { SharedModule } from '@shared/shared.module'
+import { RequestFormComponent } from './shared/request-form/request-form.component'
+import { NgxMaskModule } from 'ngx-mask'
+import { RequestsMapComponent } from './shared/requests-map/requests-map.component'
+import { RouterModule } from '@angular/router'
+import { requestsRoutes } from './requests.routes'
+import { RequestAddressFieldComponent } from './request-details/request-address-field/request-address-field.component'
 
 @NgModule({
   declarations: [
@@ -18,13 +19,14 @@ import { requestsRoutes } from './requests.routes';
     RequestsListComponent,
     RequestFormComponent,
     RequestsMapComponent,
+    RequestAddressFieldComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(requestsRoutes),
     MaterialComponentsModule,
     SharedModule,
-    NgxMaskModule.forChild(),
-  ],
+    NgxMaskModule.forChild()
+  ]
 })
 export class RequestsModule {}
